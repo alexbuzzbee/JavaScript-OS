f7cfd063b648a32a5220957f9e4d65ac9a669ef4
alexbuzzbee <alexbuzzbee@gmail.com>
2014-06-30 11:57:28 -0400

    Comments, comments, comments! And others. Added LOTS of comments, partially refactored Motherboard::getDeviceLink() and Motherboard::attachDevice(), added Motherboard::removeDevice().

--- js/host/coreHardware.js
+++ js/host/coreHardware.js
@@ -4,21 +4,35 @@ function Motherboard() {
   this.primaries = {};
   this.hasPrimaries = {};
 
+  // Returns the port of the attached device.
   this.attachDevice = function(device) {
-    this.devices.push(device);
-    if (!this.hasPrimaries[device.type]) {
-      this.changePrimary(this.devices.length - 1, device.type);
+    port = this.devices.length // The length of an array always equals the next index.
+    this.devices[port] = device;
+    if (!this.hasPrimaries[device.type]) { // If the the type of the device has no primary:
+      this.changePrimary(port, device.type); // Set the primary for this type to the attached device.
     }
-    return this.devices.length - 1
+    return port; // We know we added the device on this port, so we should return it.
   }
 
+  // Removes a device from the system. It will remain
+  this.removeDevice = function(port) {
+    delete this.devices[port]
+  }
+
+  /* Returns 1 if device on port is not the right type, 2 if there is no primary for the type.
+   Pass '""' to get any device type. */
   this.getDeviceLink = function(type, port) {
-    if (!port && this.hasPrimaries[type] == true) {
-      var port = this.primaries[type];
-    } else if (!port) {
-      return false;
+    if (!port && this.hasPrimaries[type] == true) { // If no port is specifed, and there is an appropriate primary device:
+      var port = this.primaries[type]; // Find the port for the primary of the specified type.
+    } else if (!port) { // If no port is specified:
+      return 2;
     }
-    return this.devices[port];
+    device = this.devices[port]; // Get the device to return.
+    if (device.type == type || type == "") { // If the device has the right type, or the requested type is '""':
+      return device;
+    } else {
+      return 1;
+    }
   }
 
   this.getMemoryPage = function(address) {
